3ffe47cad8734d8d64482ad0c8014d7a1e7e4742
Abel Cavalcante <38321332+FidyBack@users.noreply.github.com>
2018-11-27 16:56:54 -0200

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// Código feito a partir do proposto pelo professor Hashimoto
+
+document.addEventListener('DOMContentLoaded', function() {
+	let iframe = document.querySelector('iframe')
+	let button = document.querySelector('button')
+
+	button.addEventListener('click', function() {
+		if(iframe.requestFullscreen) {
+			iframe.requestFullscreen()
+		}
+		else if(iframe.msRequestFullscreen) {
+			iframe.msRequestFullscreen()
+		}
+		else if(iframe.mozRequestFullScreen) {
+			iframe.mozRequestFullScreen()
+		}
+		else if(iframe.webkitRequestFullscreen) {
+			iframe.webkitRequestFullscreen()
+		}
+	})
+})
